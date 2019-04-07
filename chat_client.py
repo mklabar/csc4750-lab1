2b00d82e61e502cd37d790b01e4c69b69ec769a4
Mary Kate La Bar <labarm@spu.edu>
2019-04-07 14:27:14 -0700

    this is the same as kierans changes...trying to fix merge issues

--- chat_client.py
+++ chat_client.py
@@ -1,34 +1,39 @@
 from socket import *
-import sys
+from struct import *
 
-if len(sys.argv) !=3:
-    print("\nInvalid syntax: please enter the IP address or hostname followed by the port number\n")
-    exit()
 
 #setup socket to connect to server
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-
+serverName = 'localhost'
+serverPort = int(input("Enter port number: "))
 clientSocket = socket(AF_INET, SOCK_STREAM) #TCP socket
 clientSocket.connect((serverName, serverPort))
-
+recv_len = 0
+#receive message from server, print it, close connection
 message = clientSocket.recv(1024).decode('utf-8')
 print(message)
 
 while True:
     #server passes 1/True if client is sender
     #passes 0/False if client is receiver
-    flag = int(clientSocket.recv(1))
-    if flag == 2:
-        break
-    sender = flag == 1
-
-    if(sender):
-        message = input("Enter a message: ")
-        clientSocket.send(message.encode('utf-8'))
-    else:
-       message = clientSocket.recv(1024).decode('utf-8')
-       print("Stranger:", message)
+	flag = int(clientSocket.recv(1))
+	print("FLAG:", flag)
+	
+	if(flag == 1):
+			message = input("Enter a message: ")
+			message_len = len(message)
+			#clientSocket.send(bytes([message_len & 0xFFFFFFFF])) 
+			clientSocket.send(message.encode('utf-8'))
+	elif(flag == 0):
+			#recv_len = int.from_bytes(clientSocket.recv(4), 'big')
+			#message = clientSocket.recv(recv_len).decode('utf-8')
+			message = clientSocket.recv(1024).decode('utf-8')
+			print("Stranger:", message)
+	elif flag == 2:
+		break
+	elif flag == 3:
+		message = clientSocket.recv(1024).decode("utf-8")
+		print("Server:", message)
+		
 
 print(clientSocket.recv(1024).decode("utf-8"))
 clientSocket.close()
--- chat_server.py
+++ chat_server.py
@@ -1,5 +1,7 @@
 from socket import *
-
+from struct import *
+import random
+import time
 MAX_CLIENTS = 2 
 
 #setup socket to wait for connections
@@ -8,9 +10,21 @@ serverSocket = socket(AF_INET, SOCK_STREAM) #TCP (reliable)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #make port reusable
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print('The server is ready to accept clients on port', serverPort)
+print('The server is ready to accept clients')
 
 clients = []
+quotes = ["Who's ready to fly on a zipline? I am!",
+ "Shut up baby I know it!",
+ "Bwee, hoo hoo, bwoo.",
+ "I'm sorry Dave. I just can't do that.",
+ "Error 404: Sarcasm module not found",
+ "Don't you call me a mindless philosopher you overweight glob of grease!",
+ "Would you like more butter?",
+ "VOIP",
+ "Be... Good...",
+ "Autobots, roll out!",
+ "DANGER WILL ROBINSON",
+ "At least ~your~ keyboard is dry",]
 #accept up to two connections from clients, which
 # must connect before we can move on
 for i in range(0, MAX_CLIENTS):
@@ -26,28 +40,41 @@ for i in range(0, MAX_CLIENTS):
 
 sender = 0
 msg = ""
+random.seed()
 while True:
-    clients[sender][0].send(b"1")
-    msg = clients[sender][0].recv(1024)
-    senderName = clients[sender]
-    
-    if msg.decode("utf-8") == "quit":
-        break
-
-    for i in range(0, MAX_CLIENTS):
-        if i != sender:
-            clients[i][0].send(b"0")
-            clients[i][0].send(msg)
-    
-    sender += 1
-    if sender == MAX_CLIENTS:
-        sender = 0
+	clients[sender][0].send(b"1")
+	#msg_len = int.from_bytes(clients[sender][0].recv(4), 'big')
+	#msg = clients[sender][0].recv(msg_len)
+	msg = clients[sender][0].recv(1024)
+	msg_dc = msg.decode("utf-8")
+	
+	if msg_dc == "quit":
+		break
+	
+	for i in range(0, MAX_CLIENTS):
+		if i != sender:
+			clients[i][0].send(b"0")
+			#clients[i][0].send(bytes([msg_len & 0xFFFFFFFF]))
+			clients[i][0].send(msg)
+			time.sleep(.01)
+	
+	if msg_dc[0:5] == "!bot ":
+		if "quote" in msg_dc:
+			quote = quotes[random.randint(0,11)]
+			for i in range(0, MAX_CLIENTS):
+				clients[i][0].send(b"3")
+				clients[i][0].sendall(quote.encode("utf-8"))
+				time.sleep(.01)
+	
+	sender += 1
+	if sender == MAX_CLIENTS:
+		sender = 0
 
 for i in range(0, MAX_CLIENTS):
-    print("Stranger", i+1, "disconnected")
-    clients[i][0].send(b"2")
-    clients[i][0].send(b"You have been disconnected from the chatroom")
-    clients[i][0].close()
+	print("Stranger", i+1, "disconnected")
+	clients[i][0].send(b"2")
+	clients[i][0].send(b"You have been disconnected from the chatroom")
+	clients[i][0].close()
 
 exit()
 
