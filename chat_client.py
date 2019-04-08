07f93c585236de30dfbd1a6bced8f22fc10cf9ab
Mary Kate La Bar <labarm@spu.edu>
2019-04-08 12:45:19 -0700

    Fixed formatting of commands, added complete comments

--- chat_client.py
+++ chat_client.py
@@ -2,47 +2,55 @@ from socket import *
 from struct import *
 import sys
 
+#if the command line argument is missing the IP/hostname or port number an error is printed
 if len(sys.argv) !=3:
-    print("\nInvalid syntax: please enter the IP address or hostname followed by the port number\n")
-    exit()
+	print("\nInvalid syntax: please enter the IP address or hostname followed by the port number\n")
+	exit()
 
-#setup socket to connect to server
+#setup socket to connect to server, arg 1 is IP or hostname, arg 2 is port number
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
 
 clientSocket = socket(AF_INET, SOCK_STREAM) #TCP socket
 clientSocket.connect((serverName, serverPort))
 
-recv_len = 0
-
-#receive message from server, print it, close connection
+#broadcast a welcome message to all clients
 message = clientSocket.recv(1024).decode('utf-8')
 print("\n" + message + "\n", end = "", sep = "")
 
 while True:
-    #server passes 1/True if client is sender
-    #passes 0/False if client is receiver
+    #server passes 0 if client is receiver
+    #server passes 1 if client is sender
+    #server passes 2 if client is being disconnected
+    #server passes 3 if a server message is incoming
+    #server passes 4 if a bot message is incoming
 	flag = int(clientSocket.recv(1))
 	
+	#prints message from other clients
 	if(flag == 0):
-			#recv_len = int.from_bytes(clientSocket.recv(4), 'big')
-			#message = clientSocket.recv(recv_len).decode('utf-8')
-			message = clientSocket.recv(1024).decode('utf-8')
-			print("Stranger:", message)
+		message = clientSocket.recv(1024).decode('utf-8')
+		print("Stranger:", message)
+	
+	#accepts client input message and sends to server
 	elif(flag == 1):
-			message = input("Enter a message: ")
-			message_len = len(message)
-			#clientSocket.send(bytes([message_len & 0xFFFFFFFF])) 
-			clientSocket.send(message.encode('utf-8'))
+		message = input("Enter a message: ")
+		clientSocket.send(message.encode('utf-8'))
+	
+	#client breaks to be disconnected
 	elif flag == 2:
 		break
+	
+    #prints message from server
 	elif flag == 3:
 		message = clientSocket.recv(1024).decode("utf-8")
 		print("Server:", message)
+	
+    #prints message from bot
 	elif flag == 4:
 		message = clientSocket.recv(1024).decode("utf-8")
 		print("Chatty Bot:", message)
-		
+
+#prints disconnection message and closes socket
 print(clientSocket.recv(1024).decode("utf-8"))
 clientSocket.close()
 exit()
--- chat_server.py
+++ chat_server.py
@@ -11,7 +11,11 @@ serverSocket = socket(AF_INET, SOCK_STREAM) #TCP (reliable)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #make port reusable
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
+
+#on runtime server asks for the number of clients
 MAX_CLIENTS = int(input("Please enter the number of expected clients: "))
+
+#confirmation message on server side
 print('The server is ready to accept', MAX_CLIENTS, 'clients on port', serverPort)
 
 spell = SpellChecker()
@@ -28,77 +32,126 @@ quotes = ["Who's ready to fly on a zipline? I am!",
  "Autobots, roll out!",
  "DANGER, WILL ROBINSON!",
  "At least ~your~ keyboard is dry",]
-#accept up to two connections from clients, which
-# must connect before we can move on
+
+#waits for max number of clients to connect
+#prints confirmation on server side they have connected
 for i in range(0, MAX_CLIENTS):
     connectionSocket, addr = serverSocket.accept()
     clients.append((connectionSocket, addr))
     if i < MAX_CLIENTS:
         print("Stranger", i+1, "connected")
 
-#omitting while loop means the server will run once!
-
+#broadcasts a welcome message to all clients upon connection
 for i in range(0, MAX_CLIENTS):
     clients[i][0].send(b"Welcome to the chatroom!\nType '/help' for a list of commands")
 
 sender = 0
 msg = ""
 random.seed()
+flag = "0"
+
 while True:
+	#sends 1 to client whose turn it is to send message
 	clients[sender][0].send(b"1")
-	#msg_len = int.from_bytes(clients[sender][0].recv(4), 'big')
-	#msg = clients[sender][0].recv(msg_len)
+	
+	#receives message from client
 	msg = clients[sender][0].recv(1024)
+	
+	#saves decoded message string
 	msg_dc = msg.decode("utf-8")
 	
+	#checks if message is a command or a bot call
 	if msg_dc[0] == "/" or msg_dc[0] == "!":
+		
+		#sets flag to 3 for server message
 		flag = "3"
+		
+		#breaks to disconnect users from server
 		if msg_dc == "/quit":
 			break
+		
+		#displays help message
 		elif msg_dc == "/help":
-			msg = "\n\tCommands:\n\n\t\t/help\tdisplays the list of commands\
-			\n\t\t/quit\tdisconnects all clients from the server\
-			\n\n\tBot Commands:\n\n\t\t!bot quote\tChatty Bot says a famous robot quote\
-			\n\t\t!bot time\tChatty Bot tells the current time\n\
-			!bot spellcheck\tChatty bot accepts one word and gives you corrected word recommendations\n"
+			msg = "\n\tCommands:\
+			\n\n\t/help\tdisplays the list of commands\
+			\n\t/quit\tdisconnects all clients from the server\
+			\n\n\tBot Commands:\
+			\n\n\t!bot quote\tChatty Bot says a famous robot quote\
+			\n\t!bot time\tChatty Bot tells the current time\
+			\n\t!bot spellcheck\tChatty bot gives a list of spelling\
+			\t\t\t\tcorrections for the first typed word\n"
+		
 		elif msg_dc[0:5] == "!bot ":
+			
+			#sets flag to 4 for bot message
 			flag = "4"
+			
+			#if the message contains quote, set msg to a random quote from list
 			if "quote" in msg_dc:
 				msg = quotes[random.randint(0,11)]
+			
+			#if the message contains time, set msg to current time
 			elif "time" in msg_dc:
 				msg = "The current time is " + datetime.datetime.now().strftime("%I:%M %p")
+			
+			#if the message contains spellcheck then the msg gives spelling recommendations
+			#only for the word following bot call (index 2)
 			elif "spellcheck" in msg_dc:
 				msg_array = msg_dc.split()
 				misspelled = spell.unknown(msg_array)
 				candidates = spell.candidates(msg_array[2])
-				msg = "You may mean"
+				msg = "You may mean..."
+				
+				#lists all possible correct spellings
 				for word in candidates:
 					msg = msg + " " + word
+			
+			#if called !bot but did not list proper command, msg set to bot error
 			else:
 				msg = "I'm sorry, I don't know that command yet!\
-				Type '/help' for a list of commands"
-		else:
-			msg = "Command does not exist. Type '/help' for a list of commmands"
+				\n\t    Type '/help' for a list of commands"
 		
+		#if message starts with '/' or '!' but does not match a proper command, msg set to general error
+		else:
+			msg = "Command does not exist. Type '/help' for a list of commands"
+		 
 		for i in range(0, MAX_CLIENTS):
+			#sends flag (either 3 or 4), sleeps to avoid jamming
 			clients[i][0].send(flag.encode("utf-8"))
-			clients[i][0].sendall(msg.encode("utf-8"))
 			time.sleep(.01)
+			
+			#sends message
+			clients[i][0].send(msg.encode("utf-8"))
+	
+	#if message is not a bot call or command, sends message to everyone EXCEPT sender
 	else:
 		for i in range(0, MAX_CLIENTS):
 			if i != sender:
+				#sends flag 0 to indicate receiving, sleeps to avoid jamming
 				clients[i][0].send(b"0")
-			#clients[i][0].send(bytes([msg_len & 0xFFFFFFFF]))
-				clients[i][0].send(msg)
 				time.sleep(.01)
+				
+				#sends message
+				clients[i][0].send(msg)
 		
+	#to rotate senders, sender iterates by 1
 	sender += 1
+	
+	#if sender == max, wraps around back to 0
 	if sender == MAX_CLIENTS:
-            sender = 0
+		sender = 0
 
+#client sent '/quit'
 for i in range(0, MAX_CLIENTS):
+	
+	#displays confirmation on server side
 	print("Stranger", i+1, "disconnected")
+	
+	#sends quitting flag, sleeps to avoid jamming
 	clients[i][0].send(b"2")
+	time.sleep(.01)
+	
+	#sends disconnection message
 	clients[i][0].send(b"You have been disconnected from the chatroom")
 	clients[i][0].close()
 
